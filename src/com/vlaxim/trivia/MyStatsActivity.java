e5509439cd8acbf4de92ed3f82173d453d2835f0
max21 <maxime.flasquin@gmail.com>
2014-02-19 21:33:33 +0100

    Partage du score

--- src/com/vlaxim/trivia/MyStatsActivity.java
+++ src/com/vlaxim/trivia/MyStatsActivity.java
@@ -21,11 +21,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.CursorJoiner.Result;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -82,6 +85,37 @@ public class MyStatsActivity extends Activity {
 		MyAdapter adapter = new MyAdapter(this, R.layout.row_score,
 				listAllScore);
 		liste.setAdapter(adapter);
+		
+		//Au clic sur un élément de la liste on propose de partager le score
+		liste.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+
+				Score scoreToShare = (Score) liste.getItemAtPosition(arg2);
+
+				String shareString = "Génial ! Le score que j'ai réalisé sur Trivia est de : " + Integer.toString(scoreToShare.getScore());
+				String resultatFinal = "";
+
+				shareString = shareString + resultatFinal;
+
+				Intent sendIntent = new Intent(
+						android.content.Intent.ACTION_SEND);
+
+				sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+						"Mon score Trivia");
+				sendIntent.putExtra(android.content.Intent.EXTRA_TEXT,
+						shareString);
+				sendIntent.setType("text/plain");
+
+				startActivity(Intent.createChooser(sendIntent,
+						"Partager mon score "));
+
+			}
+
+		});
+		
 
 	}
 	
